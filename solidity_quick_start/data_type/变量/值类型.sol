4ee6aedde08c745dc23b3ddaa0660ec472fcaf1e
straight <865862724@qq.com>
2024-10-19 18:49:35 +0800

    值类型中英文文档提交

--- solidity_quick_start/data_type/变量/值类型.sol
+++ solidity_quick_start/data_type/变量/值类型.sol
@@ -85,7 +85,8 @@ contract ValueTypes{
 
     // 地址
     address public _address = 0x7A58c0Be72BE218B41C608b7Fe7C5bB630736C71;
-    address payable public _address1 = payable(_address); // payable address，可以转账、查余额
+    address payable public _address1 = payable(_address);
+    // payable address，可以转账、查余额
     // 地址类型的成员
     uint256 public balance = _address1.balance; // balance of address
     
@@ -104,6 +105,33 @@ contract ValueTypes{
     // enum可以和uint显式的转换
     function enumToUint() external view returns(uint){
         return uint(action);
+    }
+
+    enum ActionChoices { GoLeft, GoRight, GoStraight, SitStill }
+    ActionChoices choice;
+    ActionChoices constant defaultChoice = ActionChoices.GoStraight;
+
+    function setGoStraight() public {
+        choice = ActionChoices.GoStraight;
+    }
+
+    // 由于枚举类型不属于ABI的一部分，
+    // "getChoice" 的签名会自动被改成 "getChoice() returns (uint8)"。
+    // 因此对于所有来自 Solidity 外部的调用，
+    function getChoice() public view returns (ActionChoices) {
+        return choice;
+    }
+
+    function getDefaultChoice() public pure returns (uint) {
+        return uint(defaultChoice);
+    }
+
+    function getLargestValue() public pure returns (ActionChoices) {
+        return type(ActionChoices).max;
+    }
+
+    function getSmallestValue() public pure returns (ActionChoices) {
+        return type(ActionChoices).min;
     }
 }
 
